d8acf1dfca1d499cdfa5f432125efdf38b02d3ee
Beans <luke.coffey538@outlook.com>
2025-10-24 22:17:04 +0100

    Squad functions

---
+++ uk1r/functions/squad/functions.hpp
@@ -0,0 +1,11 @@
+#include "script_component.hpp"
+class DOUBLES(PREFIX,COMPONENT) {
+  class functions {
+    file = QUOTE(PREFIX\functions\COMPONENT);
+    class getInManEH {};
+    class init {};
+    class getRole {};
+    class seatSwitchedManEH {};
+    class setRole {};
+  };
+};
